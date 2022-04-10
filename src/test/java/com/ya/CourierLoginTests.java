20ba8c3a4e5719e394b05aa7aecd2063864159b9
Надежда Сергеевна Бокарева <87896632+nadezdabokareva@users.noreply.github.com>
2022-04-10 21:15:10 +0300

    third commit
    after review comments

--- src/test/java/com/ya/CourierLoginTests.java
+++ src/test/java/com/ya/CourierLoginTests.java
@@ -2,166 +2,84 @@ package com.ya;
 
 import com.ya.RestClients.CourierRestClient;
 import com.ya.entity.Courier;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static com.ya.CreatingCourierTests.createRandomCourier;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 @Story("Тесты на создание курьера")
 public class CourierLoginTests {
 
-    @Test
+    private Courier courier;
+    private String safePassword;
+    private String safeLogin;
+
+    @Before
     @DisplayName("Курьер может авторизоваться")
-    public void testCourierCanLogin() {
-        Courier courier = createRandomCourier();
+    public void setUp() {
+        courier = createRandomCourier();
+        CourierRestClient.createNewRestCourier(courier);
+        ValidatableResponse validatableResponse = CourierRestClient.login(courier);
+        assertNotNull(validatableResponse.extract().body().path("id"));
+        assertEquals(200, validatableResponse.extract().statusCode());
+        safePassword = courier.getPassword();
+        safeLogin = courier.getLogin();
+    }
 
-        safetyCreateRestCourier(courier);
-
-        assertNotNull(CourierRestClient.login(courier));
-
-        safetyDeleteCourier(courier);
-
+    @After
+    @DisplayName("Курьера можно удалить")
+    public void tearDown() {
+        courier.setLogin(safeLogin);
+        courier.setPassword(safePassword);
+        ValidatableResponse validatableResponse = CourierRestClient.delete(courier);
+        assertEquals(200, validatableResponse.extract().statusCode());
+        assertEquals(true, validatableResponse.extract().body().path("ok"));
     }
 
     @Test
     @DisplayName("Курьер не может авторизоваться без логина")
     public void testLoginCourierWithoutLogin() {
-        Courier courier = createRandomCourier();
-        String courierLogin = courier.getLogin();
-
-        safetyCreateRestCourier(courier);
-
         courier.setLogin(null);
-
-        invalidLogin("Выполнен вход без логина", NullPointerException.class, courier);
-
-        courier.setLogin(courierLogin);
-
-        safetyDeleteCourier(courier);
-
-    }
-
-
-    @Test
-    @DisplayName("Курьер не может авторизоваться без пароля")
-    public void testLoginCourierWithoutPassword() {
-        Courier courier = createRandomCourier();
-        String courierPassword = courier.getPassword();
-
-        safetyCreateRestCourier(courier);
-
-        courier.setPassword(null);
-
-        invalidLogin("Выполнен вход без пароля", IllegalStateException.class, courier);
-
-        courier.setPassword(courierPassword);
-
-        safetyDeleteCourier(courier);
-
-    }
-
-    @Test
-    @DisplayName("Курьер не может авторизоваться с некорректным паролем")
-    public void testLoginCourierIncorrectPassword() {
-        Courier courier = createRandomCourier();
-        String courierPassword = courier.getPassword();
-
-        safetyCreateRestCourier(courier);
-
-        courier.setPassword("null");
-
-        invalidLogin("Выполнен вход без пароля", NullPointerException.class, courier);
-
-        courier.setPassword(courierPassword);
-
-        safetyDeleteCourier(courier);
+        ValidatableResponse validatableResponse = CourierRestClient.login(courier);
+        assertEquals(400, validatableResponse.extract().statusCode());
+        assertEquals("Недостаточно данных для входа", validatableResponse.extract().body().path("message"));
     }
 
     @Test
     @DisplayName("Курьер не может авторизоваться с некорректным логином")
     public void testLoginCourierIncorrectLogin() {
-        Courier courier = createRandomCourier();
-        String courierLogin = courier.getLogin();
-
-        safetyCreateRestCourier(courier);
-
         courier.setLogin("null");
-
-        invalidLogin("Выполнен вход без логина", NullPointerException.class, courier);
-
-        courier.setLogin(courierLogin);
-
-        safetyDeleteCourier(courier);
+        ValidatableResponse validatableResponse = CourierRestClient.login(courier);
+        assertEquals(404, validatableResponse.extract().statusCode());
+        assertEquals("Учетная запись не найдена", validatableResponse.extract().body().path("message"));
     }
+
+    @Test(expected = IllegalStateException.class)
+    @DisplayName("Курьер не может авторизоваться без пароля")
+    public void testLoginCourierWithoutPassword() {
+        courier.setPassword(null);
+        ValidatableResponse validatableResponse = CourierRestClient.login(courier);
+        assertEquals(400, validatableResponse.extract().statusCode());
+        assertEquals("Недостаточно данных для входа", validatableResponse.extract().body().path("message"));
+    }
+
     @Test
-    @DisplayName("Курьер не может авторизоваться с пустым логином")
-    public void testLoginNotRestCourier() {
-        Courier courier = createRandomCourier();
-        String courierLogin = courier.getLogin();
-
-        courier.setLogin("null");
-
-        invalidLogin("Выполнен вход без логина", NullPointerException.class, courier);
-
-        courier.setLogin(courierLogin);
-
-        safetyDeleteCourier(courier);
+    @DisplayName("Курьер не может авторизоваться с некорректным паролем")
+    public void testLoginCourierIncorrectPassword() {
+        courier.setPassword("null");
+        ValidatableResponse validatableResponse = CourierRestClient.login(courier);
+        assertEquals(404, validatableResponse.extract().statusCode());
+        assertEquals("Учетная запись не найдена", validatableResponse.extract().body().path("message"));
     }
 
     @Test
     @DisplayName("Курьер может авторизоваться и после логина возвращается id курьера")
     public void testCourierCanLoginAndHasId() {
-        Courier courier = createRandomCourier();
-
-        safetyCreateRestCourier(courier);
-
-        CourierRestClient.login(courier);
-
-        safetyDeleteCourier(courier);
-
         assertNotEquals(courier.getId(), 0);
-    }
-
-
-    @Step("Создание курьера без наполнения данными")
-    private static void safetyCreateRestCourier(Courier courier) {
-        try {
-            ValidatableResponse createResponse = CourierRestClient.createNewRestCourier(courier);
-            int statusCode = createResponse.extract().statusCode();
-            boolean creatingMessage = createResponse.extract().path("ok");
-            assertThat(statusCode, equalTo(201));
-            assertThat(creatingMessage, equalTo(true));
-        } catch (AssertionError assertionError) {
-            throw new AssertionError("Некорректно произошло создание курьера");
-        }
-    }
-
-    @Step("Удаление курьера с проверкой на ошибки")
-    protected static void safetyDeleteCourier(Courier courier){
-        try {
-            CourierRestClient.login(courier);
-            assertThat(CourierRestClient.delete(courier), equalTo(true));
-        } catch (AssertionError assertionError) {
-            throw new AssertionError("После теста курьер не был удален");
-        } catch (NullPointerException nullPointerException){
-
-        }
-    }
-
-    @Step("Авторизация курьера с проверкой на ошибки")
-    private static <T extends Exception> void invalidLogin(String message,
-                                                           Class<T> exceptionType, Courier courier) {
-        try {
-            CourierRestClient.login(courier);
-        } catch (Exception ex){
-            if (courier.getId() != 0 || !exceptionType.isInstance(ex)) {
-                throw new AssertionError(message);
-            }
-        }
     }
 }
--- src/test/java/com/ya/CreatingCourierTests.java
+++ src/test/java/com/ya/CreatingCourierTests.java
@@ -8,45 +8,61 @@ import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static com.ya.CourierLoginTests.safetyDeleteCourier;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
-
 
 @Story("Тесты на создание курьера")
 public class CreatingCourierTests {
 
+    private Courier courier;
+    private String safePassword;
+    private String safeLogin;
+
+    @Step("Генерация рандомных логина, пароля и имени для курьера")
+    protected static Courier createRandomCourier() {
+        String courierLogin = RandomStringUtils.randomAlphabetic(10);
+        String courierPassword = RandomStringUtils.randomAlphabetic(10);
+        String courierFirstName = RandomStringUtils.randomAlphabetic(10);
+        Allure.addAttachment("Логин", courierLogin);
+        Allure.addAttachment("Пароль", courierPassword);
+        Allure.addAttachment("Имя", courierFirstName);
+        return new Courier(courierLogin, courierPassword, courierFirstName);
+    }
+
+    @Before
+    public void setUp() {
+        courier = createRandomCourier();
+        CourierRestClient.login(courier);
+        safePassword = courier.getPassword();
+        safeLogin = courier.getLogin();
+    }
+
+    @After
+    public void tearDown() {
+        courier.setLogin(safeLogin);
+        courier.setPassword(safePassword);
+        CourierRestClient.delete(courier);
+    }
+
     @Test
     @DisplayName("Курьера можно создать со всеми необходимыми данными")
     public void testCreatingOneNewCourierWithValidCredentials() {
-        Courier courier = createRandomCourier();
-
-        ValidatableResponse createResponse = CourierRestClient.createNewRestCourier(courier);
-
-        assertThat(createResponse.extract().statusCode(),
-                equalTo(201));
-        assertThat(createResponse.extract().path("ok"),
-                equalTo(true));
-
-        safetyDeleteCourier(courier);
+        ValidatableResponse validatableResponse = CourierRestClient.createNewRestCourier(courier);
+        assertThat(validatableResponse.extract().statusCode(), equalTo(201));
+        assertThat(validatableResponse.extract().path("ok"), equalTo(true));
     }
 
     @Test
     @DisplayName("Нельзя создать курьера с одинаковыми логином, паролем и фамилией")
     public void testCreatingCourierClone() {
-        Courier courier = createRandomCourier();
         CourierRestClient.createNewRestCourier(courier);
-
         ValidatableResponse createResponse = CourierRestClient.createNewRestCourier(courier);
-
-        assertThat(createResponse.extract().statusCode(),
-                equalTo(409));
-        assertThat(createResponse.extract().path("message"),
-                equalTo("Этот логин уже используется. Попробуйте другой."));
-
-        safetyDeleteCourier(courier);
+        assertThat(createResponse.extract().statusCode(), equalTo(409));
+        assertThat(createResponse.extract().path("message"), equalTo("Этот логин уже используется. Попробуйте другой."));
     }
 
     @Test
@@ -54,57 +70,28 @@ public class CreatingCourierTests {
     public void testCreatingCourierWithoutLoginCredential() {
         Courier courier = createRandomCourier();
         courier.setLogin(null);
-
         ValidatableResponse createResponse = CourierRestClient.createNewRestCourier(courier);
-
-        assertThat(createResponse.extract().statusCode(),
-                equalTo(400));
-        assertThat(createResponse.extract().path("message"),
-                equalTo("Недостаточно данных для создания учетной записи"));
+        assertThat(createResponse.extract().statusCode(), equalTo(400));
+        assertThat(createResponse.extract().path("message"), equalTo("Недостаточно данных для создания учетной записи"));
     }
 
     @Test
     @DisplayName("Нельзя создать курьера без пароля")
     public void testCreatingCourierWithoutPasswordCredential() {
-        Courier courier = createRandomCourier();
         courier.setPassword(null);
-
         ValidatableResponse createResponse = CourierRestClient.createNewRestCourier(courier);
-
-        assertThat(createResponse.extract().statusCode(),
-                equalTo(400));
-        assertThat(createResponse.extract().path("message"),
-                equalTo("Недостаточно данных для создания учетной записи"));
-
+        assertThat(createResponse.extract().statusCode(), equalTo(400));
+        assertThat(createResponse.extract().path("message"), equalTo("Недостаточно данных для создания учетной записи"));
     }
 
     @Test
-    @DisplayName("Нельзя создать двух оразных курьеров с однинаковым логином")
-    public void testCreatingCourierSameLogin() throws CloneNotSupportedException {
-        Courier courier = createRandomCourier();
+    @DisplayName("Нельзя создать двух разных курьеров с однинаковым логином")
+    public void testCreatingCourierSameLogin() {
         Courier secondCourier = (Courier) courier.clone();
         secondCourier.setFirstName(RandomStringUtils.randomAlphabetic(10));
-        CourierRestClient.createNewRestCourier(courier);
-
+        CourierRestClient.createNewRestCourier(secondCourier);
         ValidatableResponse createSecondResponse = CourierRestClient.createNewRestCourier(secondCourier);
-
-        assertThat(createSecondResponse.extract().statusCode(),
-                equalTo(409));
-        assertThat(createSecondResponse.extract().path("message"),
-                equalTo("Этот логин уже используется. Попробуйте другой."));
-
-    }
-
-    @Step("Генерация рандомных логина, пароля и имени для курьера")
-    protected static Courier createRandomCourier(){
-        String courierLogin = RandomStringUtils.randomAlphabetic(10);
-        String courierPassword = RandomStringUtils.randomAlphabetic(10);
-        String courierFirstName = RandomStringUtils.randomAlphabetic(10);
-
-        Allure.addAttachment("Логин", courierLogin);
-        Allure.addAttachment("Пароль", courierPassword);
-        Allure.addAttachment("Имя", courierFirstName);
-
-        return new Courier(courierLogin, courierPassword, courierFirstName);
+        assertThat(createSecondResponse.extract().statusCode(), equalTo(409));
+        assertThat(createSecondResponse.extract().path("message"), equalTo("Этот логин уже используется. Попробуйте другой."));
     }
 }
